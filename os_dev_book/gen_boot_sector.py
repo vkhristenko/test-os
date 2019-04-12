61d07dc1a9d0e52da012da30df46cebe11cd56e9
vkhristenko <vdkhristenko1991@gmail.com>
2019-04-12 19:40:54 +0200

    able to boot with bochs and qemu

--- os_dev_book/gen_boot_sector.py
+++ os_dev_book/gen_boot_sector.py
@@ -1,11 +1,23 @@
-sector_as_str = "e9 fd ff 00 00 00 00 00 00 00 00 00 00 00 00 00" +\
-         "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00" +\
-         "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00" +\
-         "00 00 00 00 00 00 00 00 00 00 00 00 00 00 55 aa"
-sector_as_list = [int(x, 16) for x in sector_as_str.split(" ")]
+def gen_boot_sector():
+    zeroes = [int("0", 16) for i in range(512)]
+    return zeroes
 
 if __name__ == "__main__":
-    print(sector_as_list)
-    print(bytes(sector_as_list))
+    # generate boot sector 512 zeroes
+    bootSectorBytes = gen_boot_sector()
+    print("%d bytes" % len(bootSectorBytes))
+
+    # modify the first 3 bytes
+    # x86 inst for endless jump
+    bootSectorBytes[0] = int("e9", 16)
+    bootSectorBytes[1] = int("fd", 16)
+    bootSectorBytes[2] = int("ff", 16)
+
+    # modify the last 2 bytes
+    # magic number to tell bios that this is indeed a boot block and not just data
+    bootSectorBytes[-1] = int("aa", 16)
+    bootSectorBytes[-2] = int("55", 16)
+
+    # dump to binary file
     with open("boot_sector.bin", "wb") as f:
-        f.write(bytes(sector_as_list))
+        f.write(bytearray(bootSectorBytes))
