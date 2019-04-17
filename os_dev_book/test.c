e34d7a7864a04862602e96cb34f3741457b10ccc
vkhristenko <vdkhristenko1991@gmail.com>
2019-04-17 12:08:07 +0200

    add test.c with the same control structure

---
+++ os_dev_book/test.c
@@ -0,0 +1,10 @@
+void test() {
+    char value = 0;
+    char output = -1;
+    if (value <= 4)
+        output = value;
+    else if (value < 40)
+        output = value;
+    else 
+        output = value;
+}
