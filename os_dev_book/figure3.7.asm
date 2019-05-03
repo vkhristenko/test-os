f623838fdd6b624d22e7b34cf512ac2a132b5e44
vkhristenko <vdkhristenko1991@gmail.com>
2019-05-03 09:36:33 +0200

    tesing segment registers

---
+++ os_dev_book/figure3.7.asm
@@ -0,0 +1,29 @@
+;
+; a simple boot sector program that demonstrates segment offsetting
+;
+mov ah, 0xoe                        ; int 10/ah = 0eh -> scrolling teletype 
+                                    ; bios routine
+mov al, [the_secret]
+int 0x10                            ; does this print an X?
+
+mov bx, 0x7c0                       ; can't set ds directly, so set bx
+mov ds, bx                          ; then copy bx to ds
+mov al, [the_secret]
+int 0x10                            ; does this print an X
+
+mov al, [es:the_secret]             ; tell the cpu to use the es (not ds) segment
+int 0x10                            ; does this print an X
+
+mov bx, 0x7c0
+mov es, bx
+mov al, [es:the_secret]
+int 0x10                            ; does this print an X?
+
+jmp $                               ; jump forever
+
+the_secret:
+    db "X"
+
+; padding and magic BIOS number
+times 510-($-$$) db 0
+dw 0xaa55
